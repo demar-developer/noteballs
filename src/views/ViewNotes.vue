6da7056ed05be7007e009be2115199d832280864
Demar <demar.developer@gmail.com>
2023-02-19 13:45:49 -0500

    update with layout and css

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="view-notes">
+    <h1>View Notes</h1>
+    <p>View Notes</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
---
+++ src/views/ViewStats.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="view-stats">
+    <h1>View stats</h1>
+    <p>View stats</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
